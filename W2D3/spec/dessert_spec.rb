57755a5c4ec0ce9dbe59e0f7c357a371cdcde2d5
kkossally <kkossally@gmail.com>
2019-08-20 22:48:05 -0400

    Complete specs

--- W2D3/spec/dessert_spec.rb
+++ W2D3/spec/dessert_spec.rb
@@ -6,18 +6,16 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
-  let(:type) { double("type") }
-  let(:quantity) { double("quantity") }
-  subject(:dessert) { Dessert.new(type, quantity, chef) }
+  let(:chef) { double("chef", :name => "royden") }
+  subject(:dessert) { Dessert.new("pie", 5, chef) }
 
   describe "#initialize" do
     it "sets a type" do
-      expect(dessert.type).to eq(type)
+      expect(dessert.type).to eq("pie")
     end
 
     it "sets a quantity" do
-      expect(dessert.quantity).to eq(quantity)
+      expect(dessert.quantity).to eq(5)
     end
 
     it "starts ingredients as an empty array" do
@@ -25,30 +23,46 @@ describe Dessert do
     end
 
     it "raises an argument error when given a non-integer quantity" do
-      let(:quantity) { "five" }
-      expect { Dessert.new(type, quantity, chef) }.to raise_error(ArgumentError)
+      expect { Dessert.new("pie", "five", chef) }.to raise_error(ArgumentError)
     end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("cloves")
+      expect(dessert.ingredients).to include("cloves")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      expect(dessert.ingredients).to receive(:shuffle!)
+      dessert.mix!
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity"do
+    dessert.eat(2)
+    expect(dessert.quantity).to eq(3)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+    expect { dessert.eat(6) }.to raise_error
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(chef).to receive(:titleize)
+      dessert.serve
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
   end
 end
