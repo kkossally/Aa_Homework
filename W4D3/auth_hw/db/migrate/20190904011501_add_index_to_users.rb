ff4a5b570de5234761e0f6097ac74c56a6df5407
kkossally <kkossally@gmail.com>
2019-09-03 21:41:10 -0400

    Reorg W4D3 folder

--- W4D3/auth_hw/db/migrate/20190904011501_add_index_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :users, :password_digest
-
-    add_index :users, :username, unique: true
-    add_index :users, :session_token, unique: true
-  end
-end
