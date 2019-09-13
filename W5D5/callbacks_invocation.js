0036af65988e002c25085ba7015488efbd0b27d1
kkossally <kkossally@gmail.com>
2019-09-13 12:58:01 -0400

    Complete W5D5 hw part 2: function calls

--- W5D5/callbacks_invocation.js
+++ W5D5/callbacks_invocation.js
@@ -8,7 +8,6 @@ const hammertime = function(time) {
 };
 
 hammertime('9 o\'clock');
-*/
 
 const readline = require('readline');
 
@@ -22,7 +21,7 @@ function teaAndBiscuits() {
     console.log(`${answer}`);
     reader.question('Woukd you like some biscuits?: ', (answer2) => {
       console.log(`${answer2}`);
-
+      
       const one = answer === 'yes' ? 'do' : 'don\'t';
       const two = answer2 === 'yes' ? 'do' : 'don\'t';
       console.log(`So you ${one} want tea and you ${two} want biscuits.`);
@@ -33,40 +32,28 @@ function teaAndBiscuits() {
 
 teaAndBiscuits();
 
-
-/* ------------
-const readline = require("readline");
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-function teaAndBiscuits() {
-  reader.question("Would you like some tea?", function(res) {
-    console.log(`You replied ${res}.`);
-    reader.question("Would you like some biscuits?", function(res2) {
-      console.log(`You replied ${res2}.`);
-      const first = res === "yes" ? "do" : "don't";
-      const second = res2 === "yes" ? "do" : "don't";
-      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
-      reader.close();
-    });
-  });
-}
-function Cat() {
-  this.name = "Markov";
-  this.age = 3;
-}
-function Dog() {
-  this.name = "Noodles";
-  this.age = 4;
-}
-Dog.prototype.chase = function(cat) {
-  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`);
-};
-const Markov = new Cat();
-const Noodles = new Dog();
-Noodles.chase(Markov);
-Noodles.chase.call(Markov, Noodles);
-Noodles.chase.apply(Markov, [Noodles]);
-
 */
+
+
+function Cat () {
+  this.name = 'Ryoki';  
+  this.age = 5;  
+}
+
+function Dog () {
+  this.name = 'Mia';
+  this.age = 8;
+}
+
+Dog.prototype.chase = function(cat) {
+  console.log(`My name is ${this.name} and I love chasing ${cat.name}! Roof!`);
+};
+
+const ryoki = new Cat ();
+const mia = new Dog ();
+
+mia.chase(ryoki);
+
+mia.chase.call(ryoki, mia);
+
+mia.chase.apply(ryoki, [ryoki]);
