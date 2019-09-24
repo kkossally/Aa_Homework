5a87bb3526265218f1118f128b172867df172059
kkossally <kkossally@gmail.com>
2019-09-24 00:10:56 -0400

    Complete middleware hw

---
+++ W7D2/todo_solution_1/frontend/store/store.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+}
+
+function addLoggingToDispatch(store) {
+  return function (next) {
+    return function (action) {
+      console.log(`Old state: ${store.getState()}`);
+      console.log(`Action: ${action}`);
+      next(action);
+      console.log(`New state: ${store.getState()}`);
+    }
+  }
+}
+
+export default configureStore;
